b17244d39d8b55b8e77508828eda69d05c03b276
Florane <qwerty.qa912@gmail.com>
2019-09-29 01:53:14 +0700

    A ton of fixes and changes
    - Updated first character creation
    - Main command now accesile at any time
    - Changed help.dat format
    - Added 'p' operator - drops lowest rolls after 'd' operator
    - Minor fixes
    - леня блять

--- botLogic.py
+++ botLogic.py
@@ -2,6 +2,7 @@ import botBasic as bot
 import polishCalc as pol
 import time
 import re
+import threading as tr
 #--------------------------------
 def isNewMessage(user):
 	while not (bot.newMessage.get("user") == user and bot.newMessage.get("new") == True):
@@ -9,47 +10,94 @@ def isNewMessage(user):
 	bot.newMessage.update({"new":False})
 	return True
 #--------------------------------
-def createCharacter(user):
-	with open('characters/' + user + '.json', 'w+', encoding = 'utf-8') as file:
+def createCommander(user, helpListings, mainLock):
+	with open('characters/' + user + '.json', 'w+', encoding = 'utf-8') as characterFile:
+		character = {}
 		with open('flavorText/firstCharacter/createCharacter/0.dat', encoding = 'utf-8') as file:
 			bot.printMessage(file.read(), user)
-		isNewMessage(user)
+		mainLock.wait()
+		if isNewMessage(user):
+			character['flavor'] = bot.newMessage.get('message')
+		maxLevel = pol.solvePolish(pol.toPolish('40+3d6p1'))['num']
+		with open('flavorText/firstCharacter/createCharacter/1.dat', encoding = 'utf-8') as file:
+			bot.printMessage(file.read().format(maxLevel), user)
+		helpListings[0] = 'mc'
+		while mainLock.wait():
+			if isNewMessage(user):
+				stat = {}
+				statList = ['INT','REF','CHAR','TECH','LUCK','MA','BODY','EM']
+				statRegex = re.compile(r'(\S{2,4}\s+|)(\d+)')
+				char = statRegex.findall(bot.newMessage.get('message'))
+				if len(char) != 8:
+					bot.printMessage('Ошибка: Неверное кол-во вхождений', user)
+					continue
+				sum = 0
+				for entry in char:
+					isStat = False
+					for test in statList:
+						if entry[0] == test:
+							isStat = True
+							stat[test] = entry[1]
+					if not isStat:
+						for test in statList:
+							if stat.get(test) == None:
+								stat[test] = entry[1]
+					sum += int(entry[1])
+				if sum != maxLevel:
+					bot.printMessage('Ошибка: Неверная сумма вхождений', user)
+					continue
+				break
 		bot.printMessage('Новая анкета на персонажа! @id{0}'.format(user), '391442603')
+		characterFile.write(str(character))
 #--------------------------------
-def firstCharacter(user):
+def firstCharacter(user, helpListings, mainLock, errorLock):
 	with open('flavorText/firstCharacter/begin.dat', encoding = 'utf-8') as file:
 		bot.printMessage(file.read(), user)
+		helpListings[0] = 'm'
 		while 1:
+			errorLock.clear()
+			print(errorLock.is_set())
+			mainLock.wait()
 			if isNewMessage(user) and bot.newMessage["message"] == 'Зарегистрировать компанию':
 				print('test')
 				break
 			elif bot.newMessage["message"] == 'Зарегистрировать коммандора':
 				with open('flavorText/firstCharacter/characterFirst0.dat', encoding = 'utf-8') as file:
 					bot.printMessage(file.read(), user)
-				createCharacter(user)
+				createCommander(user, helpListings, mainLock)
 				break
 			else:
 				bot.printMessage('Нажмите на одну из кнопок ниже, чтобы продолжить' ,user)
+		helpListings[0] = 'm'
+		errorLock.set()
 #--------------------------------
 def diceCalc(string):
 	string = string.lower()
 	string = re.sub(r'\s+',r'',string)
 	try:
 		solved = pol.solvePolish(pol.toPolish(string))
-	except SyntaxError:
+	except:
 		return None
 	return str(solved['num']) + ' = ' + pol.cutDices(string, solved['dices'])
 #--------------------------------
 def init(user):
+	helpListings = ['mf']
+	if user[:2] == '20':
+		helpListings[0] = 'm'
+	mainLock = tr.Event()
+	errorLock = tr.Event()
+	errorLock.set()
 	while 1:
+		mainLock.clear()
 		if isNewMessage(user):
 			message = bot.newMessage["message"].title()
 			#admin = bot.newMessage.get("admin")
 			if message == 'Помощь':
+				print(helpListings)
 				answer = ''
 				with open('help.dat',encoding = 'utf-8') as file:
 					for line in file:
-						ans = re.match(r'\:\s(.*)', line)
+						ans = re.match(r'[' + helpListings[0] + ']\s(.*)', line)
 						if ans != None:
 							answer += ans.group(1) + '\n'
 				bot.printMessage(answer, user)
@@ -58,16 +106,31 @@ def init(user):
 				command = re.match(r'Помощь\s(.*)', message).group(1)
 				with open('help.dat',encoding = 'utf-8') as file:
 					found = False
+					prevListing = ''
 					for line in file:
-						if re.match(r'\:\s(.*)', line) != None:
-							if re.match(r'\:\s(.*)', line).group(1) == command:
+						print(line)
+						if re.match(r'[' + helpListings[0] + ']\s(.*)', line) != None:
+							print('1')
+							prevListing = line[0]
+							if re.match(r'[' + helpListings[0] + ']\s(.*)', line).group(1) == command:
+								print('11')
 								found = True
 							else:
+								print('12')
 								found = False
-						elif re.match(r'\:', line) != None:
+						elif re.match(r'-\s(.*)', line) != None:
+							print('2')
+							if re.match(r'-\s(.*)', line).group(1) == command and prevListing in helpListings[0]:
+								print('21')
+								found = True
+						elif re.match(r'.\s', line) != None:
+							print('3')
 							found = False
 						elif found == True:
-							answer += line
+							print('4')
+							if re.match(r'-\s', line) == None:
+								print('41')
+								answer += line
 				bot.printMessage(answer, user)
 			#--------------------
 			elif re.match(r'Бросить', message) != None or re.match(r'Roll', message) != None:
@@ -81,7 +144,13 @@ def init(user):
 			#--------------------
 			elif user[:2] != '20':
 				if message == 'Начать':
-					firstCharacter(user)
-				else:
+					tr.Thread(target = firstCharacter,args =  (user, helpListings, mainLock, errorLock)).start()
+				elif errorLock.is_set():
 					bot.printMessage('Введите "Помощь" чтобы получить список комманд', user)
-		time.sleep(0.1)
+				else:
+					bot.newMessage.update({'new':True})
+			#леня ну мать твою
+			elif message == 'Aide':
+				bot.printMessage('леня блять', user)
+		mainLock.set()
+		#time.sleep(0.1)
--- polishCalc.py
+++ polishCalc.py
@@ -16,18 +16,24 @@ def diceRoll(amount, sides):
     ret = {}
     ret['num'] = str(num)
     ret['dices'] = dices
-    return {'num':str(num), 'dices':ret}
+    return ret
 #--------------------------------
 def toPolish(string):
     stack, output = [], []
     number = ''
     for char in string:
+        if number == 'a':
+            number = ''
+        #print('1 ' + str(output))
+        #print('2 ' + str(stack))
+        #print('3 ' + str(number))
+        #print('-------')
         if re.match('[0-9]',char) != None:
             number += char
             continue
         elif number != '':
             output.append(number)
-            number = ''
+            number = 'a'
 
         if char == ')':
             a = ''
@@ -43,21 +49,27 @@ def toPolish(string):
             char = 'n'
         elif len(stack) > 0:
             if char == 'd':
-                if stack[-1] == 'd':
+                if re.match('[dp]', stack[-1]):
                     output.append(stack.pop())
+            elif char == 'p':
+                if stack[-1] == 'd':
+                    stack[-1] = 'p'
+                else:
+                    raise SyntaxError
+                continue
             elif char == '^':
-                if re.match('[d^]', stack[-1]) != None:
+                if re.match('[dp^]', stack[-1]) != None:
                     output.append(stack.pop())
             elif re.match('[*/]', char) != None:
-                if re.match('[d^*/]', stack[-1]) != None:
+                if re.match('[dp^*/]', stack[-1]) != None:
                     output.append(stack.pop())
             elif re.match('[+-]', char) != None:
-                if re.match('[d^*/+-]', stack[-1]) != None:
+                if re.match('[dp^*/+-]', stack[-1]) != None:
                     output.append(stack.pop())
         stack.append(char)
-        print(stack)
-        print(output)
 
+    if number == 'a':
+        number = ''
     if number != '':
         output.append(number)
     while len(stack) > 0:
@@ -66,41 +78,62 @@ def toPolish(string):
             raise SyntaxError
         else:
             output.append(a)
-    print(output)
+    #print(output)
     return output
 #--------------------------------
 def solvePolish(line):
     stack = []
     dices = []
     for char in line:
+        #print('b ' + str(stack))
+        #print(str(char == '-'))
         if char == 'n':
             stack[-1] = str(-int(stack[-1]))
-        elif re.match(r'[d^/*-+]', char):
+        elif re.match(r'[dp^/*\-+]', char):
             b = int(stack.pop())
             a = int(stack.pop())
             if char == 'd':
                 c = diceRoll(a, b)
-                stack.append(c['num'])
-                dices.append(c['dices'])
+                #print(c)
+                dices.append(c)
+                c = c['num']
+            elif char == 'p':
+                d = int(stack.pop())
+                #print(stack)
+                c = diceRoll(d, a)
+                i = 0
+                while i < b:
+                    min = -1
+                    pos = 0
+                    iter = 0
+                    for die in c['dices']:
+                        intDie = int(die)
+                        if min == -1 or intDie < min:
+                            pos = iter
+                            min = intDie
+                        iter += 1
+                    c['num'] = str(int(c['num']) - min)
+                    #print(c['dices'])
+                    c['dices'].pop(pos)
+                    i += 1
+                dices.append(c)
+                c = c['num']
             elif char == '^':
                 c = a ** b
-                stack.append(c)
             elif char == '/':
                 if b == 0:
                     raise SyntaxError
                 c = int(a / b)
-                stack.append(c)
             elif char == '*':
                 c = a * b
-                stack.append(c)
             elif char == '-':
                 c = a - b
-                stack.append(c)
             elif char == '+':
                 c = a + b
-                stack.append(c)
+            stack.append(c)
         else:
             stack.append(char)
+        #print('a ' + str(stack))
     ret = {}
     if len(stack) > 1:
         raise SyntaxError
@@ -109,6 +142,8 @@ def solvePolish(line):
     return ret
 #--------------------------------
 def cutDices(string, dices):
+    #print(string)
+    #print(dices)
     for dice in dices:
-        string = re.sub(r'\d+d\d+',str(dice['num']) + str(dice['dices']), string, 1)
+        string = re.sub(r'\d+d\d+p*\d*',str(dice['num']) + str(dice['dices']), string, 1)
     return string
